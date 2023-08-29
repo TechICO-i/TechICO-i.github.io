3f512d6829148d404515d298f15fb613829accc3
TechICO-i <111956739+TechICO-i@users.noreply.github.com>
2023-08-29 22:07:16 +1000

    Add files via upload

---
+++ Games/typinggame.js
@@ -0,0 +1,123 @@
+const paragraphs = [
+    "IF the way that it was considered a immoral thing which is neverthough of, why would you wish such as a thing on a person whose existance did nothing but exist?. This can't be done. However, a cake is the llama of a caravan. Snakelike armies show us how playgrounds can be viscoses. Framed in a different way, they were lost without the fatal dogsled that composed their waitress. Far from the truth, the cockney freezer reveals itself as a wiggly tornado to those who look. The first hawklike sack.",
+    "In reality, the lettuce feels more like an uncursed bacon than a folklore rabbi, which is how authors frequently misread it. Distances pursued demonstrate the charlatanism of in-laws. In reality, the lion feels more like a leprous lasagne than the cormous science that authors frequently misread as the lion. Aside from the recent scandal, their band was wearing a race-related outfit at the time. A joke's punchline transforms into a costumed chair. The original pickled chess.",
+    "The first skinny kitten is a kind of input in current times. A roast begins with an ostrich. An obstructed exhaust is a mental gun. When seen correctly, a recorder is equivalent to a grade. The cowbell of a skin is sanitary. Few people can think of a dun brazil that isn't a sophisticated playroom. The beast that is not wanted comes from a thorny oxygen. The respect of an insured benefit is accompanied by the belief that the knowledgeable specialist is a fix.",
+    "The assumption that a pig of the seashore is a hardback pilot is something we are unsure of. A darkish clave is not just an objective, contrary to what the literature would have us believe. There aren't many limbate leos that aren't silvery in the light. A mitten forms the bow. The drawer is a bay, though. Few people can identify a paunchy blue that isn't a conoid bow, in case that was unclear. Those who look will see that the undrunk railway is really just a downstage bamboo.",
+    "A bassoon is an aunt if you look at in the correct way. Some speculate that the melic myanmar is less than kutcha, as best we can tell. Foods and wacky bows are inseparable. Those who look can see that the scampish closet is actually a sclerous llama. The skirt of a peak is a hip. Some hemp-washing machines are only referred to as orchids. When viewed properly, a gum is a trumpet. A free flight is a mental headache. The croup is one theory.",
+    "When viewed properly, a baby resembles a shingle. Collars were only used during operations prior to defences. They are happy relatives, bails. The debt of a streetcar is an alloy. A lax laundry is thought to be a scarecrow warrior. It is unlikely that a stamp would qualify as a peddling payment without also being a crocodile. A talent is an enthusiast of meteorology. In this instant, the smell of them was a secret sensation. A cougar that resembles a box replaces a bacon as the rival.",
+    "A mental network is a broadband congestion. Glowing phases and chickens cannot be separated. A production is a faucet when seen properly. One may say that the lines resemble zincoid females. A deborah is the whale of the tractor. Japanese elites are cod. Some claim that the wig-like Norwegian is not particularly flashy. The explosion of a pennoned windchime is accompanied by the idea that the printed trombone is a supply. Relationships are erratic testing.",
+    "Some teeming herons have recently been reduced to abstractions like numbers. There is no debate about the fact that a marble of the mind is an unlaid fur. Few people can name a shiny liar who isn't an ungrateful bone, which is far from the truth. A giraffe is the chicken. Without the abscessed leek that made up their flock, they were helpless. Interviewers are enormous bombs. Vanward maracas demonstrate the dubious nature of scarves. Few people can think of a guled punch that isn't a pig.",
+    "If you look at it the proper way, a cough is a chat. The idea that snuffly flax is a rainbow is included in a tray that was specifically created for a tractor. At this time, their health served as an earthy passbook. This might be the case, or perhaps it could be claimed that swordfish mimic healthy sessions. If you look at a capricorn the correct way, it's like helium. Sleds, on the other hand, are mailmen's tennis. A toeless raincoat replaces an alarm as its rival. In this instance, their twist was.",
+    "The flag is frequently misinterpreted by writers as a pointless trigonometry, but in reality it feels more like bousy gold. Few people can think of a jasp oven that isn't a stammering grape. Without the huffy religion that made up their booklet, they were lost. Nothing more than pedestrians make up those waves. Most people struggle to think of a quartered semicolon that isn't a scooter. Despite our assumption, the literature would have us believe the opposite.",
+    "Few people can probably think of a pasteboard quiver that isn't a brittle alligator, but that may be this. The number of a swordfish is death. When the mist actually seems more like a crosswise closet, authors frequently mistakenly describe it as an expanding asphalt. Some claim that the tone brother-in-law is not even a newborn. We are aware that the sizes could be compared to sleepwalking cycles. Supplies were only fighters prior to seasons. At this precise time, their stew was.",
+    "The intended outcome is a lawn-like view. Dibbles are not feminine. Those who look can see that the former penalty is a busy field. Few people can think of a chalky force that isn't written by primates. But without the gamy screen that made up their beret, they were lost. There is no debate that, when viewed properly, a step-uncle is a factory. Paints are inextricably linked to gloomy windows. We are unsure of whether or not",
+    "A tramp is a siamese when viewed properly. We are aware that a monkey's fluttering jaw carries the notion that the pamphlet-like lips shatter. Right now, their cream looked like a rotten daffodil. A guest is the nest. False, they were helpless without the released fabric that made up their step-sister. It is difficult to consider a vibraphone a pardine process without also including archaeology in the definition. A hyacinth's bay develops.",
+    "One might compare the frosts to backstage chards. Colleges and pinkish bacons go together like pink and bacon. The mother of a rooster becomes a chordal hydrogen, which is untrue. It is difficult to think of a tempo as a credit that is more pure without also being pyjama. The first combined ease resembles pantyhose in its own peculiar way. By applying this reasoning, it may be claimed that the guides resemble reddest monkeies. A van is framed differently as an addle hemp..",
+    "An open reminder without catamarans is actually a foundation of smarmy semicircles, which is far from the truth. A similar board devoid of harps is actually a satin of predetermined pans. Parenting is viewed by a hubcap as a difficult beautician. Some powerful branches, according to the zeitgeist, are considered to be little more than affects. A cross is a music for poppies. Those who look can see that the valanced list is actually an exchanged wrist. Aside from recent controversy.",
+    "Those who look will see that the weighty viewpoint is a sterile peer-to-peer. This might be the case, or maybe a diamond watch changes into a baboon with a bosom. Some people now believe that the open road is not as good as it once was. Really, it is an irrefutable reality that, to those who look, the livelong lettuce exposes itself to be an empty beverage. In the past, a little was the season of balance. The morning's popcorn is transformed into a moonless splendour..",
+    "In case it wasn't clear, a friend can be compared to a refrigerator. An unsettled carriage is a mental stitch. A temper is a pair from the proper perspective, to be more precise. The liquid frequently appears in writing as a notchy baseball, but in reality, it seems more like an unbarbed angle. Despite our assumption to the contrary, the initial vagrom report is a tower in its own right. We are aware that a CD's octopus changes into an unrented dahlia.",
+    "The idea that the condemned syrup is a want brings about a repentant discussion's rest. The wallaby drake develops into a sonorous harp. If any of this was apparent, spotty kids demonstrate how jumps can be technicians. Their honey served as a time indicator at this precise moment. The lion of a hate is a ship. Without the croupous vehicle that made up their lily, they were lost. A spiral bean is thought to represent a precursor of the birth in modern times.",
+    "Those cowbells are merely components. This may be the case, or maybe ideas were only opinions before stockings. It is believed that a coil of the exclamation is a harmless toy. The cast of a religion is its board. The first stinky sailboat is, in a sense, an exchange from ancient times. Few people can think of a tutti channel that doesn't operate without feet. By applying this reasoning, an oatmeal is a shake's rooster. They are nothing more than matches, those stepsons.",
+];
+
+
+const typingText = document.querySelector(".typing-text p")
+const inpField = document.querySelector(".wrapper .input-field")
+const tryAgainBtn = document.querySelector(".try-again")
+const timeTag = document.querySelector(".time span b")
+const mistakeTag = document.querySelector(".mistake span")
+const wpmTag = document.querySelector(".wpm span")
+const cpmTag = document.querySelector(".cpm span")
+
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = mistakes = isTyping = 0;
+
+
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    paragraphs[ranIndex].split("").forEach(char => {
+        console.log(char);
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+
+function initTyping() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if (typedChar == null) {
+            if (charIndex > 0) {
+                charIndex--;
+                if (characters[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if (characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+
+
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0: wpm;
+
+
+        wpmTag.innerText = wpm;
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        clearInterval(timer);
+        inpField.value = "";
+    }
+}
+
+
+function initTimer() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+
+loadParagraph(); 
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
---
+++ Games/wordguessgame.js
@@ -0,0 +1,202 @@
+const wordList = [
+  {
+      word: "python",
+      hint: "beginner programming language"
+  },
+  {
+      word: "guitar",
+      hint: "a musical instrument"
+  },
+  {
+      word: "aim",
+      hint: "a purpose or intention"
+  },
+  {
+      word: "venus",
+      hint: "planet of our solar system"
+  },
+  {
+      word: "pink",
+      hint: "relationship colour in ALARM"
+  },
+  {
+      word: "ebay",
+      hint: "online shopping site"
+  },
+  {
+      word: "unity",
+      hint: "used to create games"
+  },
+  {
+      word: "coding",
+      hint: "action for programming"
+  },
+  {
+      word: "matrix",
+      hint: "science fiction movie"
+  },
+  {
+      word: "bugs",
+      hint: "error in programming"
+  },
+  {
+      word: "arcane",
+      hint: "Netflix game Original  series  "
+  },
+  {
+      word: "gif",
+      hint: "a file format for image"
+  },
+  {
+      word: "mental",
+      hint: "related to the mind"
+  },
+  {
+      word: "map",
+      hint: "diagram represent of an area"
+  },
+  {
+      word: "island",
+      hint: "land surrounded by water"
+  },
+  {
+      word: "hockey",
+      hint: "a famous outdoor game"
+  },
+  {
+      word: "chess",
+      hint: "related to an indoor game"
+  },
+  {
+      word: "viber",
+      hint: "a social media app"
+  },
+  {
+      word: "github",
+      hint: "code hosting platform"
+  },
+  {
+      word: "png",
+      hint: "a image file format"
+  },
+  {
+      word: "silver",
+      hint: "precious greyish-white metal"
+  },
+  {
+      word: "mobile",
+      hint: "an electronic device"
+  },
+  {
+      word: "gpu",
+      hint: "computer component"
+  },
+  {
+      word: "java",
+      hint: "medium level programming language"
+  },
+  {
+      word: "google",
+      hint: "famous search engine"
+  },
+  {
+      word: "venice",
+      hint: "famous city of waters"
+  },
+  {
+      word: "excel",
+      hint: "microsoft product for windows"
+  },
+  {
+      word: "mysql",
+      hint: "a relational database system"
+  },
+  {
+      word: "nepal",
+      hint: "developing country name"
+  },
+  {
+      word: "flute",
+      hint: "a musical instrument"
+  },
+  {
+      word: "crypto",
+      hint: "related to cryptocurrency"
+  },
+  {
+      word: "tesla",
+      hint: "unit of magnetic flux density"
+  },
+  {
+      word: "mars",
+      hint: "planet of our solar system"
+  },
+  {
+      word: "proxy",
+      hint: "related to server application"
+  },
+  {
+      word: "email",
+      hint: "related to exchanging message"
+  },
+  {
+      word: "html",
+      hint: "markup language for the web"
+  },
+  {
+    word: "css",
+    hint: "language for the web which is used for design"
+},
+  {
+      word: "air",
+      hint: "related to a gas"
+  },
+  {
+      word: "idea",
+      hint: "a thought or suggestion"
+  },
+  {
+      word: "server",
+      hint: "related to computer or system"
+  },
+  {
+      word: "svg",
+      hint: "a vector image format"
+  },
+  {
+      word: "jpeg",
+      hint: "a image file format"
+  },
+  {
+      word: "search",
+      hint: "act to find something"
+  },
+  {
+      word: "key",
+      hint: "small piece of metal"
+  },
+  {
+      word: "egypt",
+      hint: "a country that existed since ancient times"
+  },
+  {
+      word: "ALARM",
+      hint: "The structure used for writing essays"
+  },
+  {
+      word: "dubai",
+      hint: "developed country name"
+  },
+  {
+      word: "photo",
+      hint: "representation of person or scene"
+  },
+  {
+      word: "nile",
+      hint: "largest river in the world"
+  },
+  {
+      word: "rain",
+      hint: "related to a water"
+  },
+]
